71de14f8d8619b84f731e648d458ee810dffa1de
mfrasca <mariotomo@gmail.com>
2011-10-24 11:22:13 +0200

    making this toy application do something

--- lizard_svg/urls.py
+++ lizard_svg/urls.py
@@ -5,12 +5,15 @@ from django.conf.urls.defaults import url
 from django.contrib import admin
 
 from lizard_ui.urls import debugmode_urlpatterns
+from django.views.generic.simple import direct_to_template
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
     (r'^admin/', include(admin.site.urls)),
+    (r'^foo/$',             direct_to_template, {'template': 'lizard_svg/index.html'}),
+    (r'^foo/(?P<id>\d+)/$', direct_to_template, {'template': 'lizard_svg/detail.html'}),
     # url(r'^something/',
     #     direct.import.views.some_method,
     #     name="name_it"),
