96df8df4c357d37d121c2d9a84ed8960c5514156
mfrasca <mariotomo@gmail.com>
2011-10-25 15:19:44 +0200

    managing ALL of the objects, random data, two colors (the ones from the legenda).

--- lizard_svg/api/views.py
+++ lizard_svg/api/views.py
@@ -7,14 +7,15 @@ class TimeseriesView(View):
     REST view for timeseries.
     """
 
-    colors = ['#ff7f00', '#7f00ff', '#000000', '#00ff7f']
+    colors = ['#000000', '#00e600']
+
     def get(self, request):
         result = []
         level = 0
         while level < 256:
             result.append({'item': request.REQUEST['item'],
                            'timestamp': level,
-                           'color': self.colors[int(uniform(0, 4))],
+                           'color': self.colors[int(uniform(0, len(self.colors)))],
                            })
             level += int(uniform(0, 64))
         return result
