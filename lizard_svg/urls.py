deac7dc7387c4925fd9d59c0b7ef18fa55ed2df2
Gijs Nijholt <gijs.nijholt@gmail.com>
2011-10-26 16:08:45 +0200

    Mounted on / instead of /foo

--- lizard_svg/urls.py
+++ lizard_svg/urls.py
@@ -14,8 +14,7 @@ urlpatterns = patterns(
     (r'^admin/', include(admin.site.urls)),
     (r'^ui/', include('lizard_ui.urls')),
     
-    (r'^foo/$',             direct_to_template, {'template': 'lizard_svg/index.html'}),
-    (r'^foo/(?P<id>\d+)/$', direct_to_template, {'template': 'lizard_svg/detail.html'}),
+    (r'^$',             direct_to_template, {'template': 'lizard_svg/index.html'}),
     (r'^api/', include('lizard_svg.api.urls')),
     # url(r'^something/',
     #     direct.import.views.some_method,
