a752e3a1bfd2dafbf8c0b232fd7a83b141ba184e
mfrasca <mariotomo@gmail.com>
2011-11-03 17:02:13 +0100

    work in progress (also animating the height of the water).

--- lizard_svg/api/views.py
+++ lizard_svg/api/views.py
@@ -17,6 +17,8 @@ class Bootstrap(View):
             value = ''
             if request.GET['group'] in ['style:stroke', 'style:fill']:
                 value = self.colors[int(uniform(0, len(self.colors)))]
+            elif request.GET['group'] in ['height']:
+                value = str(12.5 * int(uniform(0, 8)))
             result.append({'item': request.GET['item'],
                            'timestamp': level,
                            'value': value,
